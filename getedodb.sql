d8b3c74ad092016650e0a16417113a6604d2c884
Babigdk <Babigdk@gmail.com>
2023-12-22 05:37:27 -0800

    Adding Readme file and getedodb.sql and png

--- getedodb.sql
+++ getedodb.sql
@@ -1,22 +1,161 @@
-CREATE TABLE `users` (
-  `id` integer PRIMARY KEY,
-  `email` varchar(255),
-  `full_name` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 22, 2023 at 02:34 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `getedodb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adonis_schema`
+--
+
+CREATE TABLE `adonis_schema` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL,
+  `migration_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `adonis_schema`
+--
+
+INSERT INTO `adonis_schema` (`id`, `name`, `batch`, `migration_time`) VALUES
+(1, 'database/migrations/1703146459222_users', 1, '2023-12-21 09:55:08'),
+(2, 'database/migrations/1703147357414_support_requests', 1, '2023-12-21 09:55:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adonis_schema_versions`
+--
+
+CREATE TABLE `adonis_schema_versions` (
+  `version` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `adonis_schema_versions`
+--
+
+INSERT INTO `adonis_schema_versions` (`version`) VALUES
+(2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `support_requests`
+--
 
 CREATE TABLE `support_requests` (
-  `id` integer PRIMARY KEY,
-  `user_id` varchar(255),
-  `first_name` varchar(255),
-  `last_name` varchar(255),
-  `email_address` varchar(255),
-  `support_message_title` varchar(255),
-  `support_message_text` text,
-  `file_path` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp
-);
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED DEFAULT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email_address` varchar(255) NOT NULL,
+  `support_message_title` varchar(255) NOT NULL,
+  `support_message_text` text NOT NULL,
+  `file_path` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `support_requests` (`user_id`);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `full_name`, `created_at`, `updated_at`) VALUES
+(1, 'test@example.com', 'John Doe', '2023-12-21 04:26:42', '2023-12-21 04:26:42');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `adonis_schema`
+--
+ALTER TABLE `adonis_schema`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `support_requests`
+--
+ALTER TABLE `support_requests`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `support_requests_user_id_foreign` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `adonis_schema`
+--
+ALTER TABLE `adonis_schema`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `support_requests`
+--
+ALTER TABLE `support_requests`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `support_requests`
+--
+ALTER TABLE `support_requests`
+  ADD CONSTRAINT `support_requests_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
